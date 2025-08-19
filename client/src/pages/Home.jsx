042218d3f82af3eb22a217a7459bcc40665b28c4
Sadid Alam <sadidalam14@gmail.com>
2025-08-19 21:40:39 +0530

    remove unwanted code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,7 +45,6 @@ const Home = () => {
         trigger: "#card1",
         start: "top 15%",
         end: "bottom 15%",
-        markers: true,
         scrub: true,
       },
     });
@@ -56,7 +55,6 @@ const Home = () => {
         trigger: "#card2",
         start: "top 15%",
         end: "bottom 15%",
-        markers: true,
         scrub: true,
       },
     });
@@ -67,7 +65,6 @@ const Home = () => {
         trigger: "#card3",
         start: "top 15%",
         end: "bottom 15%",
-        markers: true,
         scrub: true,
       },
     });
